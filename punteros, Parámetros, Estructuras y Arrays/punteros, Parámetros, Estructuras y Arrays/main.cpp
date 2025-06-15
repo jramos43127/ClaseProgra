52a446dbbcb3931f5d87aaa22f8aa6a568e54786
Juan Carlos Ramos Escalona <jramos43127@ucq.edu.mx>
2025-06-15 13:59:25 -0600

    creditos

--- punteros, Parámetros, Estructuras y Arrays/punteros, Parámetros, Estructuras y Arrays/main.cpp
+++ punteros, Parámetros, Estructuras y Arrays/punteros, Parámetros, Estructuras y Arrays/main.cpp
@@ -1,9 +1,11 @@
+// Juan Carlos Ramos Escalona | ISSC
 #include "ejercicio_uno.h"
 #include "ejercicio_dos.h"
 #include "ejercicio_tres.h"
 #include <iostream>
 using namespace std;
 
+// Juan Carlos Ramos Escalona | ISSC
 int main() {
 	cout << "================= Ejercicio uno =================" << endl;
 	ejercicio_uno();
